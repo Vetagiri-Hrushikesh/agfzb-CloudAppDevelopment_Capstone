7d4faf0c1f7656f0136e03d3ca3603c27c4713b1
Vetagiri-Hrushikesh <vetagirihrushikesh97@gmail.com>
2023-05-06 07:03:36 -0400

    Removed uneccesary comments

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,27 +8,7 @@ urlpatterns = [
         path('static-template/', views.static_template_view, name='static_template'),
         path('about/', views.about_template_view, name='about'),
         path('contact/', views.contact_template_view, name='contact'),
-
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-
-    # path for about view
-
-    # path for contact us view
-
-    # path for registration
-
-    # path for login
-
-    # path for logout
-
-    path(route='', view=views.get_dealerships, name='index'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
+        path(route='', view=views.get_dealerships, name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -10,11 +10,8 @@ from datetime import datetime
 import logging
 import json
 
-# Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-
-# Create your views here.
 def static_template_view(request):
     return render(request, 'djangoapp/static_template.html')
 
@@ -25,30 +22,7 @@ def about_template_view(request):
 def contact_template_view(request):
     return render(request, 'djangoapp/contact.html')
 
-# Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
-
-# Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
-
-# Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
-
-# Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/index.html', context)
-
-
-# Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
-
-# Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
-# ...
-
